c4d0d4bad686d9335b1044fd5f84d2a9cb4392a5
denis <dle@verbolia.com>
2024-06-13 15:28:10 +0200

    init chat

---
+++ chatbot.py
@@ -0,0 +1,42 @@
+from openai import OpenAI
+import re
+from config import *
+import streamlit as st
+pre_prompt=open(prompt_path).read()
+
+
+
+st.title("News Buddy")
+client = OpenAI(api_key=api_key)
+st.session_state["model"]=model
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role" : "system", "content" : "Tu es un assistant qui résumé l'actualité"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What article do you want to summarize ?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content":  "Fais-moi un résumé de cette article en quelques lignes :" + prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+with st.chat_message("assistant"):
+    stream = client.chat.completions.create(
+        model=st.session_state["model"],
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream=True,
+    )
+    response = st.write_stream(stream)
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+    
---
+++ config.py
@@ -0,0 +1,3 @@
+article_path="data/articles.txt"
+prompt_path="data/pre_prompt.txt"
+model = "gpt-3.5-turbo"
